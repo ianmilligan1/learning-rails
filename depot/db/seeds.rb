4dd81a029397c4d9570b99099ecfdb3f5f8f6247
ianmilligan1 <i2milligan@uwaterloo.ca>
2017-08-12 09:36:37 -0400

    checking diff on resolving lack of show edit destroy

--- depot/db/seeds.rb
+++ depot/db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.delete_all
+# . . . 
+Product.create!(title: 'Seven Mobile Apps in Seven Weeks',
+	description:
+		%{<p>
+			<em>Native Apps, Multiple Platforms</em>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam quis nulla pharetra, feugiat mi a, ullamcorper elit. Proin viverra risus enim, ac varius erat feugiat ut. Duis id nulla suscipit, consectetur turpis ac, rhoncus ante. Curabitur sodales dui sed magna posuere finibus. Vivamus efficitur odio in pharetra facilisis. Donec ac dictum dolor. Integer iaculis tincidunt ullamcorper. Proin id elit sit amet ligula convallis ultricies. Proin venenatis risus at sagittis iaculis. Etiam nec sem ac ex ultricies lacinia. Donec lacinia posuere congue. Aliquam pulvinar ligula et egestas interdum. Morbi varius est ac nunc suscipit, vel interdum diam pharetra.
+			</p>},
+	image_url: '7apps.jpg',
+	price: 26.00)
+# . . . 
